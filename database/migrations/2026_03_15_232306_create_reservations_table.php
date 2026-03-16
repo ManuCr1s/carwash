5e96ce331466cd273f739559f083f8cace43e0fb
manuel <mcristobaln@outlook.com>
2026-03-15 19:34:27 -0500

    feat: Add migrations table services, vehicles, reservations and states

---
+++ database/migrations/2026_03_15_232306_create_reservations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vehicle_id')->constrained('vehicles')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
+            $table->foreignId('state_id')->constrained('states')->onDelete('cascade');
+            $table->date('date_reservation');
+            $table->time('time_reservation',precision:0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
