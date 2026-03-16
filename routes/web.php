fdf2bd146a9cccffed20c0ff504381334573d238
manuel <mcristobaln@outlook.com>
2026-03-15 20:21:32 -0500

    feat: Add Page component livewire

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Livewire\User\CreateReservation;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -12,4 +13,5 @@ Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])
     })->name('dashboard');
     Route::get('/register',[UserController::class,'create'])->name('user.create');
     Route::post('/register',[UserController::class,'store'])->name('user.register');
+    Route::get('/booking/new', CreateReservation::class)->name('booking.create');
 });
