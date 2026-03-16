8816ac436ddf38212ee1f3fae4f4d7a1824f3c00
manuel <mcristobaln@outlook.com>
2026-03-15 19:35:25 -0500

    feat: Add Seeders for dates in the tables Roles and Permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class PermissionSeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $user = User::find(1);
+        $user->assignRole(1);
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $admin = Role::create(['name' => 'administrador']);
+        $cliente = Role::create(['name' => 'cliente']);
+        $usuario = Role::create(['name' => 'usuario']);
+
+        Permission::create(['name' => 'crear reserva'])->assignRole($cliente);
+        Permission::create(['name' => 'editar reserva'])->assignRole($cliente);
+        Permission::create(['name' => 'eliminar reserva'])->assignRole($cliente);
+        Permission::create(['name' => 'ver reserva'])->assignRole($cliente);
+
+        Permission::create(['name' => 'crear usuario'])->assignRole($admin);
+        Permission::create(['name' => 'editar usuario'])->assignRole($admin);
+        Permission::create(['name' => 'eliminar usuario'])->assignRole($admin);
+        Permission::create(['name' => 'ver usuario'])->assignRole($admin);
+
+
     }
 }
