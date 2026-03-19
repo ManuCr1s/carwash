331652c325002dd3d846bfe5f0ae8adb13a427c1
manuel <mcristobaln@outlook.com>
2026-03-19 16:16:04 -0500

    feat: Create page response request

---
+++ app/Livewire/User/ResponseRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User;
+
+use Livewire\Component;
+
+class ResponseRequest extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.response-request')->layout('layouts.app');
+    }
+}
