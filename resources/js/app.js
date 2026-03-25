ba1fa251adef80b413beb9d16950c2bf7e86d646
manuel <mcristobaln@outlook.com>
2026-03-25 00:01:54 -0500

    feat; Add libray sewtalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+document.addEventListener('livewire:initialized', () => {
+    Livewire.on('swal', (data) => {
+        Swal.fire(data[0]);
+    });
+});
