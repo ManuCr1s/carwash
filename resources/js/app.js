187289ed94eefd22fb168055fb003e77670a30a9
manuel <mcristobaln@outlook.com>
2026-03-26 00:03:04 -0500

    feat: Import FilePond and js y css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,59 @@
 import './bootstrap';
 import Swal from 'sweetalert2';
 window.Swal = Swal;
+
+import * as FilePond from 'filepond';
+
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import FilePondPluginImageResize from 'filepond-plugin-image-resize';
+import FilePondPluginImageTransform from 'filepond-plugin-image-transform';
+
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginFileValidateType,
+    FilePondPluginFileValidateSize,
+    FilePondPluginImageResize,
+    FilePondPluginImageTransform
+);
+
+window.FilePond = FilePond;
+
 document.addEventListener('livewire:initialized', () => {
     Livewire.on('swal', (data) => {
         Swal.fire(data[0]);
     });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    window.initFilePond = () => {
+
+        document.querySelectorAll('.filepond').forEach(input => {
+
+            if (input._pond) return;
+
+            FilePond.create(input, {
+                allowMultiple: false,
+                acceptedFileTypes: ['image/*'],
+                maxFileSize: '2MB',
+
+                imageResizeTargetWidth: 800,
+                imageTransformOutputQuality: 0.6,
+            });
+
+        });
+
+    };
+
+});
+
+document.addEventListener('init-filepond', () => {
+    setTimeout(() => {
+        window.initFilePond();
+    }, 100);
 });
