278f8d9195e28e6b0036cd8acfc5ec2845b930bc
manuel <mcristobaln@outlook.com>
2026-03-26 15:42:41 -0500

    fix: Add column lastname,dni,phone

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname')->nullable();
+            $table->string('dni', 15)->nullable()->unique();
+            $table->string('phone', 20)->nullable();
             $table->string('google_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
