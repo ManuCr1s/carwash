80ff7b3520e1c1c02127ece24fba9bd51bd4e5c2
manuel <mcristobaln@outlook.com>
2026-03-26 11:12:29 -0500

    feat: Page component for reprot reservation

---
+++ resources/views/livewire/user/report-reservation.blade.php
@@ -0,0 +1,10 @@
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Reporte de Atenciones') }}       
+        </h2>
+    </x-slot>
+    <div class="flex justify-center mt-5">
+        <div class="w-full max-w-5xl">
+
+        </div>
+    </div>
