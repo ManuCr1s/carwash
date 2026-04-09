56e6a2e220fd15c7e51ea618a6a4353b7e21c87c
manuel <mcristobaln@outlook.com>
2026-04-09 12:46:17 -0500

    feat: Modifify component upload photo

--- app/Livewire/Components/Modals/HandleReservationModal.php
+++ app/Livewire/Components/Modals/HandleReservationModal.php
@@ -15,62 +15,88 @@ class HandleReservationModal extends Component
     public $reservaId;
     public bool $show = false;
     public $service_id;
+    //Slots que se guardar en un array
     public $photos = [];
     public $date_init;
+    public $price;
+    //Slots nuevo para las 6 fotos
+    public $photo1, $photo2, $photo3, $photo4, $photo5, $photo6, $observations;
     #[On('openHandleModal')]
     public function openHandleModal($id)
     {
         $this->reservaId = $id;
 
+        //Se agrega para las 6 fotos $this->reset(['photo1', 'photo2', 'photo3', 'photo4', 'photo5', 'photo6'])
         $this->dispatch('init-filepond');   
          
         $this->show   = true;
     }
+    protected function rules(): array
+    {
+        return [
+            'observations' => 'max:255',
+            'photo1' => 'required|image|max:2048',
+            'price' =>  ['required', 'numeric', 'regex:/^\d{1,10}(\.\d{1,2})?$/']
+        ];
+    }
+    protected function messages(): array
+    {
+        return [
+            'observations.max' => 'Las observaciones no deben superar los 255 caracteres.',
+
+            'photo1.required' => 'Debe subir una imagen.',
+            'photo1.image' => 'El archivo debe ser una imagen válida (jpg, png, etc).',
+            'photo1.max' => 'La imagen no debe superar los 2MB.',
+
+            'price.required' => 'El precio es obligatorio.',
+            'price.numeric' => 'El precio debe ser un número válido.',
+            'price.regex' => 'El precio debe tener máximo 10 enteros y 2 decimales.',
+        ];
+    }
     public function processReservation()
     {
-        //n$this->validate();
-
+        $this->validate();
         // Guardar las fotos
         try {
-            $photoPaths = [];
-            foreach ($this->photos as $photo) {
-                $path = $photo->store('carwash/reservas/' . $this->reservaId, 'public');
-                $photoPaths[] = $path;
-            }
-            $user = auth()->user();
-            DB::transaction(function () use ($user, $photoPaths) {
+            DB::transaction(function () {
+                $user = auth()->user();
                 $reservation = Reservation::findOrFail($this->reservaId);
 
+                // 1. Crear la Orden
                 $order = $user->orders()->create([
-                    'reserva_id' => $this->reservaId ,
-                    'date_init' => now(),
-                    'observations' => 'Cliente habitual',
+                    'reserva_id'   => $this->reservaId,
+                    'date_init'    => now(),
+                    'observations' => $this->observations,
+                    'price' => $this->price,
                 ]);
 
-                foreach ($photoPaths as $path) {
-                    $order->photos()->create([
-                        'url_image'  => $path,
-                        'tipo_photo' => true,
-                    ]);
+                // 2. Procesar las 6 fotos
+                for ($i = 1; $i <= 6; $i++) {
+                    $property = "photo$i";
+                    if ($this->$property) {
+                        $path = $this->$property->store('carwash/reservas/' . $this->reservaId, 'public');
+                        
+                        $order->photos()->create([
+                            'url_image'  => $path,
+                            'type_photo' => true, // Antes del lavado
+                        ]);
+                    }
                 }
 
-                $reservation->update([
-                    'state_id' => 2 
-                ]);
+                // 3. Actualizar estado de reserva
+                $reservation->update(['state_id' => 2]);
             });
-            
 
             $this->show = false;
-            $this->photos = [];
-            // Evento para limpiar FilePond en JS
             $this->dispatch('tableRefresh');
             $this->dispatch('swal', [
                 'title' => '¡Completado!',
-                'text' => 'La reserva ha sido atendida con éxito.',
+                'text' => 'Vehículo registrado, ¡a lavar!',
                 'icon' => 'success'
             ]);
+
         } catch (\Exception $e) {
-            dd($e->getMessage());
+            $this->dispatch('swal', ['title' => 'Error', 'text' => $e->getMessage(), 'icon' => 'error']);
         }
         
     }
