dcf2d86079d5f0e4eaacd1efda6cf2414b6728c8
manuel <mcristobaln@outlook.com>
2026-04-09 16:34:13 -0500

    fix: Bug fixed query table

--- app/Livewire/Components/Tables/ReportReservationTable.php
+++ app/Livewire/Components/Tables/ReportReservationTable.php
@@ -20,7 +20,9 @@ class ReportReservationTable extends LivewireTable
       return Reservation::query()
             ->join('services', 'reservations.service_id', '=', 'services.id')
             ->join('vehicles', 'reservations.vehicle_id', '=', 'vehicles.id')
-            ->select('services.name', 'vehicles.modelo','vehicles.marca','vehicles.placa','reservations.date_reservation','reservations.time_reservation')
+            ->join('models', 'vehicles.model_id', '=', 'models.id')
+            ->join('brands','models.brand_id','=','brands.id')
+            ->select('services.name', 'models.name as models_name','brands.name as brands_name','vehicles.placa','reservations.date_reservation','reservations.time_reservation')
             ->where('reservations.state_id','=',3);
     }
     #[On('tableRefresh')]
@@ -29,9 +31,9 @@ class ReportReservationTable extends LivewireTable
         return [
             Column::make(__('ID'),'id'),
             Column::make(__('Name'), 'name'),
-            Column::make(__('Marca'), 'marca'),
+            Column::make(__('Marca'), 'brands_name'),
             Column::make(__('Placa'), 'placa'),
-            Column::make(__('Modelo'), 'modelo'),   
+            Column::make(__('Modelo'), 'models_name'),   
             Column::make(__('Dia'), 'date_reservation'),
             Column::make(__('Hora'), 'time_reservation'),
             ViewColumn::make('Acciones','components.table-report-action'),
